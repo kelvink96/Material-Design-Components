91d7caf0b13e2677b3cd3b97e108873a75aa504b
Kelvin <kelvin.kiprop96@gmail.com>
2019-03-04 13:56:05 +0300

    Changes

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+import {MDCRipple} from "@material/ripple";
+
 const shippingForm = document.querySelector('#crane-shipping-form');
 shippingForm.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-  alert('Success!');
+    evt.preventDefault();
+    alert('Success!');
 });
+new MDCRipple(document.querySelector('.mdc-button'));
